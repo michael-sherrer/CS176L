9282319e9d387888c7e850ca2bedf2d897c4a80b
michael-sherrer <114097258+michael-sherrer@users.noreply.github.com>
2023-09-07 16:08:25 -0400

    My first java program version 1

---
+++ hello.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+public class hello 
+{
+
+	public static void main(String[] args) 
+	{
+		//CS176L
+		//Michael Sherrer
+		//helloWorld
+		System.out.println("Hello World");
+	}
+
+}
