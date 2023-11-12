e4909402f5306c07bdcef7733065906a92e745ad
michael-sherrer <114097258+michael-sherrer@users.noreply.github.com>
2023-11-12 15:42:22 -0500

    Interface Assignment Version 2

---
+++ Data.java
@@ -0,0 +1,63 @@
+package edu.monmouth.interfaceAssignment;
+
+public class Data
+{
+   /**
+      Computes the average of the measures of the given objects.
+      @param objects an array of Measurable objects
+      @return the average of the measures
+   */
+   public static double average(Measurable[] objects)
+   {
+      double sum = 0;
+      for (Measurable obj : objects)
+      {
+         sum = sum + obj.getMeasure();
+      }
+      if (objects.length > 0)
+      {
+         return sum / objects.length;
+      }
+      else
+      {
+         return 0;
+      }
+   }
+
+   /**
+      Computes the maximum of the measures of the given objects.
+      @param objects an array of Measurable objects
+      @return the maximum of the measures, null if array is empty
+   */
+   public static double max(Measurable[] objects)
+   {
+      if (objects.length == 0)
+      {
+         return 0;
+      }
+      //Measurable max = objects[0];
+      double max = 0;
+      for (Measurable obj : objects)
+      {
+         if (obj.getMeasure() > max)
+         {
+            max = obj.getMeasure();
+         }
+      }
+
+      return max;
+   }
+   public static double min(Measurable[] objects) {
+	      if (objects.length == 0) {
+	         return 0;
+	      }
+	      double min = objects[0].getMeasure();
+	      for (Measurable obj : objects) {
+	         if (obj.getMeasure() < min) {
+	            min = obj.getMeasure();
+	         }
+	      }
+	      return min;
+	   }
+	}
+
--- MeasurableTester.java
+++ MeasurableTester.java
@@ -1,3 +1,9 @@
+package edu.monmouth.interfaceAssignment;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
    This program demonstrates the measurable Country class.
 */
@@ -5,52 +11,50 @@ public class MeasurableTester
 {
    public static void main(String[] args)
    {
-     
-	  //Countries
-	  System.out.println("Countries Data");
-	  Measurable[] countries = new Measurable[3];
-      countries[0] = new Country("Uruguay", 176220);
-      countries[1] = new Country("Thailand", 513120);
-      countries[2] = new Country("Belgium", 30510);
-      double avg=Data.average(countries);
-      System.out.println("Average area: " + avg);
-      System.out.println("Expected: 239950");
-      System.out.println("");
-      double maxData = Data.max(countries);
-      System.out.println("Maximum area: " + maxData);
-      System.out.println("Expected: 513120");
-      System.out.println("");
-     
-      //Bank Accounts
-      System.out.println("Bank account data");
-      Measurable[] bank = new Measurable[3];
-      bank[0] = new BankAccount(176220);
-      bank[1] = new BankAccount(513120);
-      bank[2] = new BankAccount(30510);
-      avg=Data.average(bank);
-      System.out.println("Average balance: " + avg);
-      System.out.println("Expected: 239950");
-      System.out.println("");
-      maxData = Data.max(bank);
-      System.out.println("Maximum balance: " + maxData);
-      System.out.println("Expected: 513120");
-      
-      // Quizzes
-      System.out.println("Quiz Data");
-      Measurable[] quizzes = new Measurable[5];
-      quizzes[0] = new Quiz(95);
-      quizzes[1] = new Quiz(80);
-      quizzes[2] = new Quiz(90);
-      quizzes[3] = new Quiz(50);
-      quizzes[4] = new Quiz(75);
-      
-      avg = Data.average(quizzes);
-      System.out.println("Average Score: " + avg);
-      System.out.println("expected: 78");
-      System.out.println("");
-      
-      maxData = Data.max(quizzes);
-      System.out.println("Maximum score: " + maxData);
-      System.out.println("Expected: 95");
+	   JFrame frame = new JFrame("Measurable Tester");
+
+       JPanel panel = new JPanel();
+
+       JButton avgButton = new JButton("Calculate Average");
+       JButton maxButton = new JButton("Calculate Maximum");
+       JButton minButton = new JButton("Calculate Minimum");
+
+       panel.add(avgButton);
+       panel.add(maxButton);
+       panel.add(minButton);
+
+       frame.add(panel);
+       frame.setSize(300, 150);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setVisible(true);
+
+       Measurable[] countries = new Measurable[3];
+       countries[0] = new Country("Uruguay", 176220);
+       countries[1] = new Country("Thailand", 513120);
+       countries[2] = new Country("Belgium", 30510);
+
+       avgButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               double avg = Data.average(countries);
+               JOptionPane.showMessageDialog(null, "Average area: " + avg);
+           }
+       });
+
+       maxButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               double maxData = Data.max(countries);
+               JOptionPane.showMessageDialog(null, "Maximum area: " + maxData);
+           }
+       });
+
+       minButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               double minData = Data.min(countries);
+               JOptionPane.showMessageDialog(null, "Minimum area: " + minData);
+           }
+       });
    }
 }
